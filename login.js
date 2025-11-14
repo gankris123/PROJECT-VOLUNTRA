cf73726422247c10e63215c4c9237b20de88ba40
justinquidit12 <justinquidit56@gmail.com>
2025-11-14 14:23:35 +0800

    added if else to function email and pass

--- login.js
+++ login.js
@@ -7,5 +7,16 @@ function login() {
     if (email === "" || pass === "") {
         message.textContent = "Please enter both email and password.";
         return;
+    
+}
+
+if (email === "admin@edu.com" && pass === "1234") {
+        message.style.color = "green";
+        message.textContent = "Login successful! Redirecting...";
+        setTimeout(() => {
+            window.location.href = "index.html";
+        }, 1200);
+    } else {
+        message.textContent = "Invalid email or password.";
     }
 }
